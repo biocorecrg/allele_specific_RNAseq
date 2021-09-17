9f93624d49293219765468d7c595361b8a7daf5c
Luca Cozzuto <lucacozzuto@gmail.com>
2021-09-17 14:11:10 +0200

    fixing names

--- bin/join_counts.sh
+++ bin/join_counts.sh
@@ -10,10 +10,10 @@ if [ x"$1" == x ]; then
 
 fi
 
-echo -e "gene_id\tgenoA\tgenoB\tref\tamb" > ${1}_group.counts
-join ${1}_alleleA_s.counts ${1}_alleleB_s.counts | join - ${1}_ref_s.counts | join - ${1}_ambiguous_s.counts | tr ' ' '\t' >> ${1}_group.counts 
+echo -e "gene_id\tgenoA\tgenoB\tunknown\tamb" > ${1}_group.counts
+join ${1}_alleleA_s.counts ${1}_alleleB_s.counts | join - ${1}_unknown_s.counts | join - ${1}_ambiguous_s.counts | tr ' ' '\t' >> ${1}_group.counts 
 
-echo -e "#fname\tgenoA\tgenoB\tref\tamb" "" > ${1}_group.stats
+echo -e "#fname\tgenoA\tgenoB\tunknown\tamb" "" > ${1}_group.stats
 
-awk -v fname=$1 '{if ($1!="_" || $1!="gene_id") {alla+=$2; allb+=$3; refs+=$4; ambs+=$5; }} END {print fname"\t"alla"\t"allb"\t"refs"\t"ambs}' ${1}_group.counts >> ${1}_group.stats
+awk -v fname=$1 'BEGIN {alla=allb=unknown=ambs=0} {if ($1!="_" || $1!="gene_id") {alla+=$2; allb+=$3; unknown+=$4; ambs+=$5; }} END {print fname"\t"alla"\t"allb"\t"unknown"\t"ambs}' ${1}_group.counts >> ${1}_group.stats
 
