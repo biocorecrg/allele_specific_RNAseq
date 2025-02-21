6b3f8ae075c02a913dfd9a2b157b267952611cd8
Luca Cozzuto <lucacozzuto@gmail.com>
2025-02-21 22:04:40 +0100

    fixing reports

--- bin/join_counts.sh
+++ bin/join_counts.sh
@@ -15,5 +15,5 @@ join ${1}_alleleA_s.counts ${1}_alleleB_s.counts | join - ${1}_unknown_s.counts 
 
 echo -e "#fname\tgenoA\tgenoB\tunknown\tamb" "" > ${1}_group.stats
 
-awk -v fname=$1 'BEGIN {alla=allb=unknown=ambs=0} {if ($1!="_" || $1!="gene_id") {alla+=$2; allb+=$3; unknown+=$4; ambs+=$5; }} END {print fname"\t"alla"\t"allb"\t"unknown"\t"ambs}' ${1}_group.counts >> ${1}_group.stats
+awk -v fname=$1 'BEGIN {alla=allb=unknown=ambs=0} {if ($0!~"_" && $1!="gene_id") {alla+=$2; allb+=$3; unknown+=$4; ambs+=$5; }} END {print fname"\t"alla"\t"allb"\t"unknown"\t"ambs}' ${1}_group.counts >> ${1}_group.stats
 
